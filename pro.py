63668e88baaf9251276054d71d8fbbfbae27d9f3
Naveen Kannan Periyachi <155178737+NaveenKANNAN2005@users.noreply.github.com>
2025-01-17 09:58:39 +0530

    Update pro.py

--- pro.py
+++ pro.py
@@ -12,7 +12,7 @@ import seaborn as sns
 st.title("Air Quality Prediction - Model Evaluation")
 
 
-uploaded_file = st.file_uploader("E:/Air-Quality-Prediction/Data/Real-Data/Real_Combine.csv", type=["csv"])
+uploaded_file = st.file_uploader("Real_Combine.csv", type=["csv"])
 
 if uploaded_file is not None:
     
